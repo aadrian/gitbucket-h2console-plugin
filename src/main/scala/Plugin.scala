b3fd7b6e445f100f7089e0e12cd88b62e96c4e38
aadrian <a.adrian.tech@googlemail.com>
2018-01-30 01:12:48 +0100

    next version number

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -7,7 +7,9 @@ class Plugin extends gitbucket.core.plugin.Plugin {
   override val pluginId: String = "h2console-plugin"
   override val pluginName: String = "H2 Console Plugin"
   override val description: String = "Add H2 Console to the administration console."
-  override val versions: Seq[Version] = List(new Version("1.0.0"))
+  override val versions: Seq[Version] = List(
+    new Version("1.0.0"),
+    new Version("1.1.0"))
 
   override val controllers = Seq(
     "/h2console/*" -> new H2ConsoleController()
